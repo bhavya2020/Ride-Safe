c15dd76bf05b044389f7e6c402974bb001f16e92
bhavya <bhavya2b@gmail.com>
2018-04-11 01:34:56 +0530

    sockets for start and stop monitoring of driver distractions
    Signed-off-by: bhavya <bhavya2b@gmail.com>

--- app/src/main/java/com/example/bhavya/safego/driversAdapter.java
+++ app/src/main/java/com/example/bhavya/safego/driversAdapter.java
@@ -1,7 +1,10 @@
 package com.example.bhavya.safego;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,22 +15,33 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URISyntaxException;
+
+import io.socket.client.Ack;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
 /**
  * Created by bhavya on 9/4/18.
  */
 
-public class driversAdapter  extends RecyclerView.Adapter<driversAdapter.driversAdapterViewHolder> {
+public class driversAdapter extends RecyclerView.Adapter<driversAdapter.driversAdapterViewHolder> {
 
+    private SharedPreferences mPrefs;
     private JSONArray drivers;
     private Context mContext;
+    private final static String ip = "192.168.1.4";
+    private final static String port = "5555";
 
-    public driversAdapter(JSONArray drivers, Context context){
-        this.drivers=drivers;
-        mContext=context;
+    public driversAdapter(JSONArray drivers, Context context) {
+        this.drivers = drivers;
+        mContext = context;
     }
+
     @Override
     public driversAdapter.driversAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        int layoutId=R.layout.driver;
+        int layoutId = R.layout.driver;
         View view = LayoutInflater.from(mContext).inflate(layoutId, parent, false);
 
         view.setFocusable(true);
@@ -39,16 +53,30 @@ public class driversAdapter  extends RecyclerView.Adapter<driversAdapter.drivers
     public void onBindViewHolder(driversAdapter.driversAdapterViewHolder holder, int position) {
 
 
-        String driverName="";
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+
+        String driverName = "";
 
         try {
-            driverName=drivers.getString(position);
+            driverName = drivers.getString(position);
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        boolean Show = mPrefs.getBoolean(driverName, true);
+        Log.i(driverName, String.valueOf(Show));
+        if (!Show) {
+            holder.monitorDriverBtn.setVisibility(View.VISIBLE);
+            holder.stopMonitorDriverBtn.setVisibility(View.GONE);
+        } else {
+            holder.monitorDriverBtn.setVisibility(View.GONE);
+            holder.stopMonitorDriverBtn.setVisibility(View.VISIBLE
+            );
+        }
+        holder.driverName.setText(driverName.substring(0, driverName.length() - 10));
+        holder.monitorDriverBtn.setTag(R.id.email, driverName);
+        holder.stopMonitorDriverBtn.setTag(R.id.email, driverName);
 
-        holder.driverName.setText(driverName);
-        holder.monitorDriverBtn.setTag(driverName);
+
     }
 
     @Override
@@ -60,19 +88,109 @@ public class driversAdapter  extends RecyclerView.Adapter<driversAdapter.drivers
 
         final TextView driverName;
         final Button monitorDriverBtn;
+        final Button stopMonitorDriverBtn;
 
 
         driversAdapterViewHolder(View view) {
             super(view);
 
-            driverName=view.findViewById(R.id.driverName);
-            monitorDriverBtn=view.findViewById(R.id.monitorDriverBtn);
+            driverName = view.findViewById(R.id.driverName);
+            monitorDriverBtn = view.findViewById(R.id.monitorDriverBtn);
+            stopMonitorDriverBtn = view.findViewById(R.id.StopMonitorDriverBtn);
 
+            monitorDriverBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SharedPreferences.Editor ed = mPrefs.edit();
+                    Log.i("true", monitorDriverBtn.getTag(R.id.email).toString());
+                    ed.putBoolean(monitorDriverBtn.getTag(R.id.email).toString(), true);
+                    ed.apply();
+
+                    stopMonitorDriverBtn.setVisibility(View.VISIBLE);
+                    monitorDriverBtn.setVisibility(View.GONE);
+
+                    final Socket socket;
+                    try {
+                        socket = IO.socket("http://" + ip + ":" + port);
+                        Log.i("socket", socket.toString());
+                        socket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+
+                            @Override
+                            public void call(Object... args) {
+                                Log.i("ss", args[0].toString());
+                            }
+                        });
+                        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+                            @Override
+                            public void call(Object... args) {
+                                socket.emit("monitor", monitorDriverBtn.getTag(R.id.email).toString(), new Ack() {
+                                    @Override
+                                    public void call(Object... args) {
+                                        Log.i("monitor", "monitoring");
+                                    }
+                                });
+                                socket.disconnect();
+                            }
+
+                        });
+                        socket.connect();
+
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            });
+            stopMonitorDriverBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SharedPreferences.Editor ed = mPrefs.edit();
+                    Log.i("false", stopMonitorDriverBtn.getTag(R.id.email).toString());
+                    ed.putBoolean(stopMonitorDriverBtn.getTag(R.id.email).toString(), false);
+                    ed.apply();
+                    stopMonitorDriverBtn.setVisibility(View.GONE);
+                    monitorDriverBtn.setVisibility(View.VISIBLE);
+                    final Socket socket;
+                    try {
+                        socket = IO.socket("http://" + ip + ":" + port);
+                        Log.i("socket", socket.toString());
+                        socket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+
+                            @Override
+                            public void call(Object... args) {
+                                Log.i("ss", args[0].toString());
+                            }
+                        });
+                        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+                            @Override
+                            public void call(Object... args) {
+                                socket.emit("stopMonitor", stopMonitorDriverBtn.getTag(R.id.email).toString(), new Ack() {
+                                    @Override
+                                    public void call(Object... args) {
+                                        Log.i("stop-monitor", "stopping");
+                                    }
+                                });
+                                socket.disconnect();
+                            }
+
+                        });
+                        socket.connect();
+
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
             view.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
+
 
         }
     }
